b3a1f1cf53f9d19da244e5e412b651f4a4d175e5
Deepak Malviya <deepak.malviya@hoonartek.com>
2024-01-05 16:32:37 +0530

    updated models

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,50 +1,26 @@
 import mongoose,{Schema} from "mongoose";
 
-const userSchema =new Schema(
-    {
-        username:{
-            type:String,
-            required:true,
-            unique:true,
-            lowercase:true,
-            trim:true,
-            index:true
-        },
-        email :{
-            type:String,
-            required:true,
-            unique:true,
-            lowercase:true,
-            trim:true,
-        },
-        fullname:{
-            type:String,
-            required:true,
-            trim:true,
-            index:true
-        },
-        avatar:{
-            type:String, //cloudinary url
-            required:true,
-        },
-        coverImage:{
-            type:String,
-
-        },
-        watchHistory:[{
-            type:Schema.Types.ObjectId,
-            ref:"Video"
-        }],
-        password:{
-            type:String,
-            required:[true,"Password is required"]
-        },
-        refreshToken:{
-            type:String
-        }
-
-    },{
-        timestamps:true
-    }
-)
-export const User=mongoose.model("User",userSchema)
+const videoSchema=new Schema({
+    videoFile:{
+        type:String,
+        required:true
+    },
+    thumbnail:{
+        type:String,
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    duration:{
+        type:Number,
+        required:true
+    },
+},{
+    timestamps:true
+})
