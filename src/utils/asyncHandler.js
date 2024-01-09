59cec4ae004f43898d0ccf277060df05c1138d0a
Deepak Malviya <deepak.malviya@hoonartek.com>
2024-01-09 17:04:38 +0530

    adding more models and update controller

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,21 +1,27 @@
-
-
 const asyncHandler = (requestHandler) => {
     return (req, res, next) => {
-        console.log("hi");
         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
+
+
 export { asyncHandler }
 
 
-// const asyncHandler = (fn) => async (req,res,next) => {
-//     try{
-//         await fn(req,res,next)
-//     }catch(error){
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
 //         res.status(err.code || 500).json({
-//             success:false,
-//             message:err.message
+//             success: false,
+//             message: err.message
 //         })
 //     }
 // }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,25 +1,41 @@
-import {v2 as cloudinary} from 'cloudinary';
-import fs from 'fs';      
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key:process.env.CLOUDINARY_API_KEY, 
-  api_secret:process.env.CLOUDINARY_API_SECRET 
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloudinary= async (localFilePath) => {
-    try{
-        if(!localFilePath)return null;
-        const response=await cloudinary.uploader.upload(localFilePath,{
-            resource_type:"auto",
-          
-        })
-        console.log("file is uploaded on cloudinary ",response.url );
-        return response;
-        
-    }catch(error){
-        fs.unlinkSync(localFilePath);
-        return null;
-    }
-}
 
-export {uploadOnCloudinary}
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      throw new Error("Local file path is not provided");
+    }
+
+    // Upload the file on Cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    // File has been uploaded successfully
+    console.log("File is uploaded on Cloudinary:", response.url);
+
+    // Remove the locally saved temporary file
+    fs.unlinkSync(localFilePath);
+
+    return response;
+  } catch (error) {
+    // Handle errors
+    console.log(error);
+    console.error("Error during file upload:", error.message);
+
+    // Remove the locally saved temporary file as the upload operation failed
+    fs.unlinkSync(localFilePath);
+
+    return null;
+  }
+};
+// console.log()
+export { uploadOnCloudinary };
